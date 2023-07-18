485b1210249f924390290bce885cc8c047d5c5bd
Fouad Haddad <40925051+fouad20-meet@users.noreply.github.com>
2023-07-18 12:41:47 +0200

    Update app.py

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -14,10 +14,10 @@ image_link = "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de254f85
 user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
 
 posts = {
-    "https://i.imgur.com/1dSgGnG.png": "The cohort of 2022!",
-    "https://i.imgur.com/CPEvMk0.jpg": "MEET graduation!",
-    "https://i.imgur.com/Cb7LK9o.jpg": "#MEET_HACKATHON",
-    "https://i.imgur.com/S5A93Wt.jpg": "Class of 2024's Y1 closing event cohort"}
+    "https://imageio.forbes.com/blogs-images/samarmarwan/files/2018/03/MEET-Students-1200x800.jpg": "Group projects <3",
+    "https://uploads-ssl.webflow.com/5dd64bd3a930f96c82bd137a/63024ce64d943673cb004a4c_2022.07.17%20-%20Summer%20Day%201.png": "MEET summer!",
+    "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de6d502d6d70c0ad49a060c_6.jpg": "#MEET_DU",
+    "https://global-uploads.webflow.com/5fe28feebcae602620061802/5fe5401840671a36cd1d47d5_5de6d5024dd1a74670173aed_1-p-1080.jpeg": "Our lovely TAs!"}
 
 
 #####
